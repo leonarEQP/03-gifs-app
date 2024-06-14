c47d1971dc200874360a6a61326af741adfbfa09
LeonarEQP <leonar.estupinanquinonez@plexus.es>
2024-06-14 14:44:59 +0200

    Lady Load apartado 108

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -9,7 +9,11 @@ export class GifsService {
   private serviceUrl: string = 'https://api.giphy.com/v1/gifs';
   public gifList: Gif[] = [];
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+    this.loadLocalStorage();
+
+    console.log('Gifs Services OK');
+  }
 
   get tagsHistory() {
     return [...this._tagsHistory];
@@ -23,6 +27,20 @@ export class GifsService {
     }
     this._tagsHistory.unshift(tag);
     this._tagsHistory = this._tagsHistory.splice(0, 10);
+    this.saveLocalStorage();
+  }
+
+  // Local Storage
+
+  private saveLocalStorage(): void {
+    localStorage.setItem('history', JSON.stringify(this._tagsHistory));
+  }
+  private loadLocalStorage(): void {
+    if (!localStorage.getItem('history')) return;
+    // this._tagsHistory = JSON.parse(localStorage.getItem('history')!);
+    this._tagsHistory = JSON.parse(localStorage.getItem('history')!);
+    if (this._tagsHistory.length === 0) return;
+    this.searchTag(this._tagsHistory[0]);
   }
 
   public searchTag(tag: string): void {
